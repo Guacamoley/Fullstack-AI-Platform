ec3afaec98293f74bc8c275186ec9418c4a737e4
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-11-09 21:34:32 -0600

    Create sidebar toggle feature for smaller screens

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,14 +3,20 @@
 import { Menu } from "lucide-react";
 
 import { Button } from "./ui/button";
-import { Sheet } from "./ui/sheet";
+import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
+import Sidebar from "./sidebar";
 
 const MobileSidebar = () => {
   return (
     <Sheet>
-      <Button variant={"ghost"} size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <SheetTrigger>
+        <Button variant={"ghost"} size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className="p-0">
+        <Sidebar />
+      </SheetContent>
     </Sheet>
   );
 };
