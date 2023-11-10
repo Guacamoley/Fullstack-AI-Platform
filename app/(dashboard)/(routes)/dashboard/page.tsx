8701565218583821e2aff006b626f7a745626271
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-11-09 22:02:42 -0600

    Create initial dashboard page code

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 const DashboardPage = () => {
   return (
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Discover the capabilities of artificial intelligence
         </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Engage in conversations with the most intelligent AI and explore the
+          capabilities of artificial intelligence.
+        </p>
       </div>
+      <div className="px-4 md:px20 lg:px-32 space-y-4"></div>
     </div>
   );
 };
