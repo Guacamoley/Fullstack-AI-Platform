dc4b2fd4672f693773b74233a8c1bfa93d5439b1
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-11-11 00:43:38 -0600

    Update empty component for new pages

--- components/empty.tsx
+++ components/empty.tsx
@@ -36,3 +36,25 @@ export const EmptyImage = ({ label }: EmptyProps) => {
     </div>
   );
 };
+
+export const EmptyMusic = ({ label }: EmptyProps) => {
+  return (
+    <div className="h-full p-20 flex flex-col items-center justify-center">
+      <div className="relative h-72 w-72">
+        <Image alt="Empty" fill src="/music.svg" />
+      </div>
+      <p className="text-muted-foreground text-sm text-center">{label}</p>
+    </div>
+  );
+};
+
+export const EmptyVideo = ({ label }: EmptyProps) => {
+  return (
+    <div className="h-full p-20 flex flex-col items-center justify-center">
+      <div className="relative h-72 w-72">
+        <Image alt="Empty" fill src="/video.svg" />
+      </div>
+      <p className="text-muted-foreground text-sm text-center">{label}</p>
+    </div>
+  );
+};
